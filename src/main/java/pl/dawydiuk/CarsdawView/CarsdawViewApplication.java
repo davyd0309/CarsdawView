61cd6a02560264b24151556ce8daad960dab6636
Konrad Dawydiuk <konrad.dawydiuk@gmail.com>
2021-05-07 16:07:15 +0200

    Initial commit

---
+++ src/main/java/pl/dawydiuk/CarsdawView/CarsdawViewApplication.java
@@ -0,0 +1,13 @@
+package pl.dawydiuk.CarsdawView;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsdawViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsdawViewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dawydiuk/CarsdawView/CarsdawViewApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dawydiuk.CarsdawView;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsdawViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
